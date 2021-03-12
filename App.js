40d94987c0fa0614bfbc071d84ece70bf28bf3a8
Nathan Waite <natew821@gmail.com>
2021-03-12 14:48:35 -0700

    Reusable list item built and icon built

--- App.js
+++ App.js
@@ -15,13 +15,14 @@ import Icon from "./app/components/Icon";
 // import Card from "./app/components/Card";
 // import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   const { landscape } = useDeviceOrientation();
 
   return (
     <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -7,18 +7,27 @@ import {
   TouchableHighlight,
 } from "react-native";
 import Colors from "../config/Colors";
+import AppText from "../components/AppText/AppText";
 
 import Swipeable from "react-native-gesture-handler/Swipeable";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  onPress,
+  renderRightActions,
+  ImageComponent,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={Colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image source={image} style={styles.image} />
+          {ImageComponent}
+          {image && <Image source={image} style={styles.image} />}
           <View style={styles.textContainer}>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.subTitle}>{subTitle}</Text>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -29,19 +38,21 @@ const styles = StyleSheet.create({
   container: {
     padding: 15,
     flexDirection: "row",
-    alignItems: "center",
   },
   image: {
     height: 70,
     width: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   title: {
     fontWeight: "500",
   },
   subTitle: {
     color: Colors.medium,
+  },
+  textContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
 });
 export default ListItem;
