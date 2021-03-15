a43596baa54a65453e939722c873ac85e81b5251
Nathan Waite <natew821@gmail.com>
2021-03-15 15:12:30 -0600

    Getting permission to use photos from the ImagePicker API

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import {
   useDeviceOrientation,
   useDimensions,
 } from "@react-native-community/hooks";
-import React, { useState } from "react";
-import ListItem from "./app/components/ListItem";
-// import Screen from "./app/components/Screen";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+// import ListItem from "./app/components/ListItem";
+import Screen from "./app/components/Screen";
 // import Icon from "./app/components/Icon";
 // import LoginScreen from "./app/screens/LoginScreen";
 // import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import ListingEditScreen from "./app/screens/ListingEditScreen";
 // import { View } from "react-native";
 // import ViewImageScreen from "./app/screens/ViewImageScreen";
 // import WelcomeScreen from "./app/screens/WelcomeScreen";
@@ -18,7 +19,7 @@ import ListingEditScreen from "./app/screens/ListingEditScreen";
 // import AppText from "./app/components/AppText/AppText";
 // import Card from "./app/components/Card";
 // import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+// import MessagesScreen from "./app/screens/MessagesScreen";
 // import ListItem from "./app/components/ListItem";
 // import MyAccountScreen from "./app/screens/MyAccountScreen";
 // import ListingsScreen from "./app/screens/ListingsScreen";
@@ -27,5 +28,12 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 // import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access your photos.");
+  };
+  useEffect(async () => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
