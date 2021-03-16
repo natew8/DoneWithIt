d07ae364a1f4cfad0c336665f58937991bb59764
Nathan Waite <natew821@gmail.com>
2021-03-16 13:23:03 -0600

    Added Nvigation Container and Stack Navigator

--- App.js
+++ App.js
@@ -1,37 +1,33 @@
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
-import React, { useEffect, useState } from "react";
-import { Button, Image } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-// import ListItem from "./app/components/ListItem";
+import React from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
 import Screen from "./app/components/Screen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-// import Icon from "./app/components/Icon";
-// import LoginScreen from "./app/screens/LoginScreen";
-// import RegisterScreen from "./app/screens/RegisterScreen";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-// import { View } from "react-native";
-// import ViewImageScreen from "./app/screens/ViewImageScreen";
-// import WelcomeScreen from "./app/screens/WelcomeScreen";
 
-// import { MaterialCommunityIcons } from "@expo/vector-icons";
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
 
-// import AppText from "./app/components/AppText/AppText";
-// import Card from "./app/components/Card";
-// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-// import MessagesScreen from "./app/screens/MessagesScreen";
-// import ListItem from "./app/components/ListItem";
-// import MyAccountScreen from "./app/screens/MyAccountScreen";
-// import ListingsScreen from "./app/screens/ListingsScreen";
-// import { Switch, Text, TextInput } from "react-native";
-// import AppTextInput from "./app/components/AppTextInput";
-// import AppPicker from "./app/components/AppPicker";
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="Tweet Details" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
