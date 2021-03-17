9070b6abf3b492f15e2dd786c76501186289e0b7
Nathan Waite <natew821@gmail.com>
2021-03-17 13:56:34 -0600

    Added icon and styling to tabs navigator

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
 
@@ -65,8 +66,23 @@ const Settings = () => (
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Feed" component={Tweets} />
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "red",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={Tweets}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
     <Tab.Screen name="Account" component={Account} />
     <Tab.Screen name="Profile" component={Profile} />
     <Tab.Screen name="Settings" component={Settings} />
